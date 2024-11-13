4e6e8772775cbb7e52d74a24f8a5ee0d83980ce8
mmichelotti <m.michelotti@hotmail.it>
2024-11-13 13:11:25 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <GLM/glm.hpp>
+#include <GLM/mat4x4.hpp>
+
+#include <iostream>
+
+int main()
+{
+	//line
+	//line2
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+
+	uint32_t extensionsCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, nullptr);
+	printf("Extensions count : %i\n", extensionsCount);
+	glm::mat4 testMatrix(1.0f);
+	glm::vec4 testVector(1.0f);
+	auto testResault = testMatrix * testVector;
+
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
